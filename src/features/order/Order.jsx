4c6649e94c40d30927dee203c0777aae2a688e7f
MPS Vihanga <>
2024-04-17 10:04:38 +0530

    writing data with react router actions

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -42,7 +42,8 @@ import {useLoaderData} from "react-router-dom";
 function Order() {
     const order = useLoaderData()
 
-    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address,
+    // these are only for the restaurant staff
     const {
         id,
         status,
