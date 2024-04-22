9391fafa91e44beb00aa068f5ece54aa3e7bb289
MPS Vihanga <>
2024-04-22 18:55:34 +0530

    building the cart page

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,58 +1,63 @@
-import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {clearCart, getCart, getName} from "./cartSlice.js";
+import EmptyCart from "./EmptyCart.jsx";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+/*const fakeCart = [
+    {
+        pizzaId: 12,
+        name: 'Mediterranean',
+        quantity: 2,
+        unitPrice: 16,
+        totalPrice: 32,
+    },
+    {
+        pizzaId: 6,
+        name: 'Vegetale',
+        quantity: 1,
+        unitPrice: 13,
+        totalPrice: 13,
+    },
+    {
+        pizzaId: 11,
+        name: 'Spinach and Mushroom',
+        quantity: 1,
+        unitPrice: 15,
+        totalPrice: 15,
+    },
+];*/
 
 function Cart() {
-  const username = useSelector(state => state.user.username);
-  const cart = fakeCart;
+    const username = useSelector(getName);
+    const cart = useSelector(getCart);
+    const dispatch = useDispatch();
 
-  return (
-    <div className="px-4 py-3">
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+    if (!cart.length) return <EmptyCart/>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
-      <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
-        ))}
-      </ul>
+    return (
+        <div className="px-4 py-3">
+            <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <div className="mt-6 space-x-2">
-        <Button to="/order/new" type="primary">
-          Order pizzas
-        </Button>
+            <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
-        <Button type="secondary">Clear cart</Button>
-      </div>
-    </div>
-  );
+            <ul className="mt-3 divide-y divide-stone-200 border-b">
+                {cart.map((item) => (
+                    <CartItem item={item} key={item.key}/>
+                ))}
+            </ul>
+
+            <div className="mt-6 space-x-2">
+                <Button to="/order/new" type="primary">
+                    Order pizzas
+                </Button>
+
+                <Button type="secondary" onClick={() => dispatch(clearCart)}>Clear cart</Button>
+            </div>
+        </div>
+    );
 }
 
 export default Cart;
